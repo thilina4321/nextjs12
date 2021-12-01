973503548e51e3e3e0ae5e1818ffe6d4093ec072
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-12-01 17:37:34 +0530

    added

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+type Product = {
+  id: string;
+  name: string;
+};
+
+const Home: React.FC<{ products: Product[] }> = (props) => {
+  const { products } = props;
+  return (
+    <div>
+      Home
+      {products.map((product) => (
+        <li key={product.id}> {product.name} </li>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      products: [{ id: "1", name: "Product 1" }],
+    },
+  };
+};
